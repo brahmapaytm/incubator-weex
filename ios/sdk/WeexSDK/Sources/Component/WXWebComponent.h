12419b1d71b829e16204af8641b74e4861f869a3
Abhinav Kumar Roy <abhinav.roy@paytm.com>
2020-05-15 13:01:21 +0530

    WXWebComponent-  Replaced UIWebView with WKWebView

--- ios/sdk/WeexSDK/Sources/Component/WXWebComponent.h
+++ ios/sdk/WeexSDK/Sources/Component/WXWebComponent.h
@@ -18,8 +18,9 @@
  */
 
 #import "WXComponent.h"
+#import <WebKit/WebKit.h>
 
-@interface WXWebComponent : WXComponent<UIWebViewDelegate>
+@interface WXWebComponent : WXComponent<WKNavigationDelegate, WKScriptMessageHandler>
 
 - (void)notifyWebview:(NSDictionary *) data;
 
